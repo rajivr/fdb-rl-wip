d5632d2283cee6dbe16ff337e2f4716ebc4d1c54
Rajiv Ranganath <rajiv.ranganath@atihita.com>
2023-01-11 18:51:14 +0530

    wip-08

--- src/record_version.rs
+++ src/record_version.rs
@@ -27,8 +27,9 @@ use fdb::tuple::Versionstamp;
 /// version and local version can be managed using [`RecordContext`].
 ///
 /// [`RecordContext`]: crate::RecordContext
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Eq, Ord, PartialOrd)]
 pub struct RecordVersion {
+    // *Note:*: This ordering is based on `Versionstamp`.
     incarnation_version: Option<u64>,
     /// Corresponds to `tr_version` field in the [`Versionstamp`]
     /// struct. In RecordLayer `FDBRecordVersion` class, this field is
@@ -41,7 +42,27 @@ pub struct RecordVersion {
 }
 
 impl RecordVersion {
-    /// TODO: Documentation + tests
+    /// Get incarnation version. When no incarnation version is
+    /// present, then `None` is returned.
+    pub fn get_incarnation_version(&self) -> Option<u64> {
+        self.incarnation_version
+    }
+
+    /// Get [`Versionstamp`].
+    pub fn get_versionstamp(&self) -> Versionstamp {
+        if self.complete {
+            // Safety: In the `From::from` implementation, we take in
+            //         a properly constructed versionstamp, which we
+            //         then deconstruct to create a value of
+            //         `RecordVersion` type. We can therefore safely
+            //         put back a value of Versionstamp without
+            //         panicing.
+            Versionstamp::complete(self.global_version.clone(), self.local_version)
+        } else {
+            Versionstamp::incomplete(self.local_version)
+        }
+    }
+
     pub(crate) fn into_parts(self) -> (Option<u64>, Bytes, u16, bool) {
         let RecordVersion {
             incarnation_version,
@@ -55,7 +76,6 @@ impl RecordVersion {
 }
 
 impl From<(u64, Versionstamp)> for RecordVersion {
-    /// TODO: Documentation + Tests
     fn from(value: (u64, Versionstamp)) -> RecordVersion {
         let (i, vs) = value;
 
@@ -73,7 +93,6 @@ impl From<(u64, Versionstamp)> for RecordVersion {
 }
 
 impl From<Versionstamp> for RecordVersion {
-    /// TODO: Documentation + Tests
     fn from(value: Versionstamp) -> RecordVersion {
         let (global_version, local_version, complete) = value.into_parts();
 
@@ -88,5 +107,195 @@ impl From<Versionstamp> for RecordVersion {
 
 #[cfg(test)]
 mod tests {
-    // TODO
+    use bytes::{BufMut, Bytes, BytesMut};
+
+    use fdb::tuple::Versionstamp;
+
+    use super::RecordVersion;
+
+    #[test]
+    fn get_incarnation_version() {
+        let rv = RecordVersion::from(Versionstamp::incomplete(10));
+        assert_eq!(rv.get_incarnation_version(), None);
+
+        let rv = RecordVersion::from((0, Versionstamp::incomplete(10)));
+        assert_eq!(rv.get_incarnation_version(), Some(0));
+    }
+
+    #[test]
+    fn get_versionstamp() {
+        let tr_version = {
+            let mut b = BytesMut::new();
+            b.put_u64(1066);
+            b.put_u16(1);
+            Bytes::from(b)
+        };
+
+        let rv = RecordVersion::from(Versionstamp::incomplete(10));
+        assert_eq!(rv.get_versionstamp(), Versionstamp::incomplete(10));
+
+        let rv = RecordVersion::from(Versionstamp::complete(tr_version.clone(), 10));
+        assert_eq!(
+            rv.get_versionstamp(),
+            Versionstamp::complete(tr_version, 10)
+        );
+    }
+
+    #[test]
+    fn comparisons() {
+        // In 10 bytes transaction version below, `1066` and `1766`
+        // are the 8 byte commit version and `1` and `2` are the
+        // transaction batch order.
+        let version_bytes_one = {
+            let mut b = BytesMut::new();
+            b.put_u64(1066);
+            b.put_u16(1);
+            Bytes::from(b)
+        };
+
+        let version_bytes_two = {
+            let mut b = BytesMut::new();
+            b.put_u64(1066);
+            b.put_u16(2);
+            Bytes::from(b)
+        };
+
+        let version_bytes_three = {
+            let mut b = BytesMut::new();
+            b.put_u64(1766);
+            b.put_u16(1);
+            Bytes::from(b)
+        };
+
+        let version_bytes_four = {
+            let mut b = BytesMut::new();
+            b.put_u64(1766);
+            b.put_u16(2);
+            Bytes::from(b)
+        };
+
+        // Each block corresponds to similar tests in Java
+        // RecordLayer.
+        {
+            assert_eq!(
+                RecordVersion::from(Versionstamp::incomplete(5)),
+                RecordVersion::from(Versionstamp::incomplete(5))
+            );
+        }
+
+        {
+            assert_ne!(
+                RecordVersion::from(Versionstamp::incomplete(5)),
+                RecordVersion::from(Versionstamp::incomplete(6))
+            );
+            assert!(
+                RecordVersion::from(Versionstamp::incomplete(5))
+                    < RecordVersion::from(Versionstamp::incomplete(6))
+            );
+            assert!(
+                RecordVersion::from(Versionstamp::incomplete(5))
+                    > RecordVersion::from(Versionstamp::incomplete(0))
+            );
+        }
+
+        {
+            assert_eq!(
+                RecordVersion::from(Versionstamp::complete(version_bytes_two.clone(), 10)),
+                RecordVersion::from(Versionstamp::complete(version_bytes_two.clone(), 10)),
+            );
+        }
+
+        {
+            assert_ne!(
+                RecordVersion::from(Versionstamp::complete(version_bytes_three.clone(), 10)),
+                RecordVersion::from(Versionstamp::complete(version_bytes_two.clone(), 10)),
+            );
+            assert!(
+                RecordVersion::from(Versionstamp::complete(version_bytes_three.clone(), 10))
+                    > RecordVersion::from(Versionstamp::complete(version_bytes_two.clone(), 10))
+            );
+            assert!(
+                RecordVersion::from(Versionstamp::complete(version_bytes_two.clone(), 10))
+                    < RecordVersion::from(Versionstamp::complete(version_bytes_three.clone(), 10))
+            );
+        }
+
+        {
+            assert_ne!(
+                RecordVersion::from(Versionstamp::complete(version_bytes_one.clone(), 10)),
+                RecordVersion::from(Versionstamp::complete(version_bytes_two.clone(), 10)),
+            );
+            assert!(
+                RecordVersion::from(Versionstamp::complete(version_bytes_one.clone(), 10))
+                    < RecordVersion::from(Versionstamp::complete(version_bytes_two.clone(), 10))
+            );
+            assert!(
+                RecordVersion::from(Versionstamp::complete(version_bytes_two.clone(), 10))
+                    > RecordVersion::from(Versionstamp::complete(version_bytes_one.clone(), 10))
+            );
+        }
+
+        {
+            assert_ne!(
+                RecordVersion::from(Versionstamp::complete(version_bytes_two.clone(), 15)),
+                RecordVersion::from(Versionstamp::complete(version_bytes_two.clone(), 10)),
+            );
+            assert!(
+                RecordVersion::from(Versionstamp::complete(version_bytes_two.clone(), 15))
+                    > RecordVersion::from(Versionstamp::complete(version_bytes_two.clone(), 10))
+            );
+            assert!(
+                RecordVersion::from(Versionstamp::complete(version_bytes_two.clone(), 10))
+                    < RecordVersion::from(Versionstamp::complete(version_bytes_two.clone(), 15))
+            );
+        }
+
+        {
+            assert_ne!(
+                RecordVersion::from(Versionstamp::incomplete(10)),
+                RecordVersion::from(Versionstamp::complete(version_bytes_one.clone(), 10)),
+            );
+            assert!(
+                RecordVersion::from(Versionstamp::incomplete(10))
+                    > RecordVersion::from(Versionstamp::complete(version_bytes_one.clone(), 10)),
+            );
+            assert!(
+                RecordVersion::from(Versionstamp::complete(version_bytes_one.clone(), 10))
+                    < RecordVersion::from(Versionstamp::incomplete(10))
+            );
+        }
+
+        // Incarnation version tests. Not present in Java RecordLayer.
+        {
+            assert_ne!(
+                RecordVersion::from(Versionstamp::complete(version_bytes_four.clone(), 11)),
+                RecordVersion::from((0, Versionstamp::complete(version_bytes_one.clone(), 10)))
+            );
+            assert!(
+                RecordVersion::from(Versionstamp::complete(version_bytes_four.clone(), 11))
+                    < RecordVersion::from((
+                        0,
+                        Versionstamp::complete(version_bytes_one.clone(), 10)
+                    ))
+            );
+            assert!(
+                RecordVersion::from((0, Versionstamp::complete(version_bytes_one.clone(), 10)))
+                    > RecordVersion::from(Versionstamp::complete(version_bytes_four.clone(), 11))
+            );
+            assert!(
+                RecordVersion::from((0, Versionstamp::complete(version_bytes_four.clone(), 11)))
+                    < RecordVersion::from((
+                        1,
+                        Versionstamp::complete(version_bytes_one.clone(), 10)
+                    ))
+            );
+            assert!(
+                RecordVersion::from((1, Versionstamp::complete(version_bytes_one.clone(), 10)))
+                    > RecordVersion::from((
+                        0,
+                        Versionstamp::complete(version_bytes_four.clone(), 11)
+                    ))
+            );
+        }
+    }
 }
